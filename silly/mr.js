891d95380a5c3a6da68a4d595c552a044cb919a5
6zd <73720662+6zd@users.noreply.github.com>
2023-01-03 12:10:45 -0500

    Update mr.js

--- silly/mr.js
+++ silly/mr.js
@@ -1,57 +1,60 @@
 // take body to change the content
 const body = document.getElementsByTagName('body');
 // stop keyboard shortcuts
+
+
 window.addEventListener("keydown", (event) => {
-    
   if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
      event.preventDefault();
      body[0].innerHTML = "nice try retrad <3 - bazooka :3";
      setTimeout(function() {
         window.location.replace("https://www.google.com/");
     }, 1);
-    window.close();
   }
+  window.close()
   if(event.ctrlKey && (event.key === "C")) {
      event.preventDefault();
      body[0].innerHTML = "nice try retrad <3 - bazooka :3";
      setTimeout(function() {
         window.location.replace("https://www.google.com/");
     }, 1);
-    window.close(); 
   }
+  window.close()
   if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
      event.preventDefault();
      body[0].innerHTML = "nice try retrad <3 - bazooka :3";
      setTimeout(function() {
         window.location.replace("https://www.google.com/");
     }, 1);
-    window.close();
   }
+  window.close()
   if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
      event.preventDefault();
      body[0].innerHTML = "nice try retrad <3 - bazooka :3";
      setTimeout(function() {
         window.location.replace("https://www.google.com/");
     }, 1);
-    window.close();
   }
+  window.close()
   if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
      event.preventDefault();
      body[0].innerHTML = "nice try retrad <3 - bazooka :3";
      setTimeout(function() {
         window.location.replace("https://www.google.com/");
     }, 1);
-    window.close();
   }
+  window.close()
   if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
      event.preventDefault();
      body[0].innerHTML = "nice try retrad <3 - bazooka :3";
      setTimeout(function() {
         window.location.replace("https://www.google.com/");
     }, 1);
-    window.close();
   }
+  window.close()
 });
+
+
 // stop right click
 document.addEventListener('contextmenu', function(e) {
   e.preventDefault();
