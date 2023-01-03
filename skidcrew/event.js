c2507224c0dc5ac0ca905bc955b2cb13755c9a25
6zd <73720662+6zd@users.noreply.github.com>
2023-01-03 11:55:22 -0500

    Update event.js

--- skidcrew/event.js
+++ skidcrew/event.js
@@ -4,28 +4,52 @@ const body = document.getElementsByTagName('body');
 window.addEventListener("keydown", (event) => {
   if(event.ctrlKey && (event.key === "S" || event.key === "s")) {
      event.preventDefault();
-     body[0].innerHTML = "u failed the nazi party..."
+     body[0].innerHTML = "nice try retrad <3 - bazooka :3"
+     setTimeout(function() {
+        window.location.replace("http://pornhub.com");
+        window.close()
+    }, 100);
   }
 
   if(event.ctrlKey && (event.key === "C")) {
      event.preventDefault();
-     body[0].innerHTML = "u failed the nazi party..."
-  }
+     body[0].innerHTML = "nice try retrad <3 - bazooka :3"
+     setTimeout(function() {
+        window.location.replace("http://pornhub.com");
+        window.close()
+    }, 100);
+  }console.time
   if(event.ctrlKey && (event.key === "E" || event.key === "e")) {
      event.preventDefault();
-     body[0].innerHTML = "u failed the nazi party..."
+     body[0].innerHTML = "nice try retrad <3 - bazooka :3"
+     setTimeout(function() {
+        window.location.replace("http://pornhub.com");
+        window.close()
+    }, 100);
   }
   if(event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
      event.preventDefault();
-     body[0].innerHTML = "u failed the nazi party...";
+     body[0].innerHTML = "nice try retrad <3 - bazooka :3";
+     setTimeout(function() {
+        window.location.replace("http://pornhub.com");
+        window.close()
+    }, 100);
   }
   if(event.ctrlKey && (event.key === "K" || event.key === "k")) {
      event.preventDefault();
-     body[0].innerHTML = "u failed the nazi party...";
+     body[0].innerHTML = "nice try retrad <3 - bazooka :3";
+     setTimeout(function() {
+        window.location.replace("http://pornhub.com");
+        window.close()
+    }, 100);
   }
   if(event.ctrlKey && (event.key === "U" || event.key === "u")) {
      event.preventDefault();
-     body[0].innerHTML = "u failed the nazi party...";
+     body[0].innerHTML = "nice try retrad <3 - bazooka :3";
+     setTimeout(function() {
+        window.location.replace("http://pornhub.com");
+        window.close()
+    }, 100);
   }
 });
 // stop right click
