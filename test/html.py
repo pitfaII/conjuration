0908804e9336d61e1813dce7eeef703d6f8d8bda
6zd <73720662+6zd@users.noreply.github.com>
2023-03-12 01:51:21 -0500

    dfffffffffffff

--- test/html.py
+++ test/html.py
@@ -1,8 +1,10 @@
-import undetected_chromedriver as uc
-from selenium import webdriver
+#import undetected_chromedriver as uc
+#from selenium import webdriver
 
-options = webdriver.ChromeOptions() 
-driver = uc.Chrome(options=options)
-driver.get('https://skidcrew.lol')
-input()
-print(driver.page_source)
+#options = webdriver.ChromeOptions() 
+#driver = uc.Chrome(options=options)
+#driver.get('https://skidcrew.lol')
+#input()
+#print(driver.page_source)
+import requests
+print(requests.get('https://skidcrew.lol/style.css').text)
